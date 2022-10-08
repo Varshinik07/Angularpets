5e656d078c0a3d5e360ff6a3647cb9627520859b
Varshini <varshuk01@gmail.com>
2022-10-08 21:11:51 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,18 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { PetsvaritiesComponent } from './petsvarities/petsvarities.component';
 import { LoginComponent } from './login/login.component';
-
+import {RouterModule,Routes} from '@angular/router';
+const appRoutes:Routes=[
+  {
+    path:"",component:HomeComponent
+  },
+  {
+    path:"pets varieties",component:PetsvaritiesComponent
+  },
+  {
+    path:"login",component:LoginComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +29,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
